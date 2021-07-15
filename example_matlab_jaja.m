208afaf3fa7b7422b598d5085865d73341f263d2
javier3297 <jamara6@uv.es>
2021-07-15 10:42:55 +0200

    Programa .m per a provar

---
+++ example_matlab_jaja.m
@@ -0,0 +1,19 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Javier Martinez Amaya, javier.martinez-amaya@uv.es 
+% https://www.aiforoceans.org/
+% ISP, MALOC
+%
+% Description: PROGRAMA GIT tutorial 2
+% 
+% 2021
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+y=[1 432 65 213 -45 65 43 -43 -67 453 0];
+x=[1 2 3 4 5 6 7 8 9 10 11];
+
+figure
+plot(x,y,'.b','MarkerSize',5)
+xlabel('Counts'), ylabel('Ni idea (nose^2)')
+why()
+figure
+spy
