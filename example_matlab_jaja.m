2bf9c7d670de1e9f608e6047c235b17d87729050
javier3297 <jamara6@uv.es>
2021-07-15 10:49:46 +0200

    Comments and code explanations

--- example_matlab_jaja.m
+++ example_matlab_jaja.m
@@ -11,9 +11,17 @@
 y=[1 432 65 213 -45 65 43 -43 -67 453 0];
 x=[1 2 3 4 5 6 7 8 9 10 11];
 
+% Important figure showing results obtained above
 figure
-plot(x,y,'.b','MarkerSize',5)
+plot(x,y,'.b','MarkerSize',10)
 xlabel('Counts'), ylabel('Ni idea (nose^2)')
+
+% Non sense functions I found today
 why()
 figure
 spy
+
+% Explain what I did
+disp('1. Showed results')
+disp('2. Answer stupid question')
+disp('3. Showed an interesting dog')
